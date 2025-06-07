2eab26dafa3e6f770b9e925aa2ba12090fe3ba53
Egor1158 <egor115819@gmail.com>
2025-06-07 07:23:34 +0300

    new bolid code

--- bolid_code/sketch_may14a/sketch_may14a.ino
+++ bolid_code/sketch_may14a/sketch_may14a.ino
@@ -4,141 +4,171 @@ QTRSensors qtr;
 const uint8_t SensorCount = 8;
 uint16_t sensorValues[SensorCount];
 
-// Пины моторов
-#define LEFT_MOTOR_PIN1 2
-#define LEFT_MOTOR_PIN2 3
-#define RIGHT_MOTOR_PIN1 4
-#define RIGHT_MOTOR_PIN2 5
-#define LEFT_MOTOR_PWM 9
-#define RIGHT_MOTOR_PWM 10
+const int AIN1 = 6;
+const int AIN2 = 9;
+const int BIN1 = 11;
+const int BIN2 = 10;
+const int STBY = 8;
+const int LIGHT = 12;
 
-#define START_BUTTON 7
+const int RED = 7;
+const int GREEN = 5;
+const int BLUE = 3;
 
-// Переменные
+const int startButton= 3;
+
 bool isRunning = false;
 int lastError = 0;
 int integral = 0;
-int currentLeftSpeed = 0;
-int currentRightSpeed = 0;
 
-const int BASE_SPEED = 150;
-float Kp = 0.4, Ki = 0.01, Kd = 0.3; // Подстроенные PID-параметры
+const int baseSpeedL = 120;
+const int baseSpeedR = 120;
+float Kp = 0.053;
+float Ki = 0;
+float Kd = 0.69;
+
+int stopCount = 0;
+int crossLineCount = 0;
+const int blackLinekLim = 800;
+const int minPosition = 300;
+const int maxPosition = 6700;
+const int stopLimit = 20;
+const int countStopLine = 2;
 
 void setup() {
   qtr.setTypeAnalog();
   qtr.setSensorPins((const uint8_t[]){A0, A1, A2, A3, A4, A5, A6, A7}, SensorCount);
 
-  pinMode(LEFT_MOTOR_PIN1, OUTPUT);
-  pinMode(LEFT_MOTOR_PIN2, OUTPUT);
-  pinMode(RIGHT_MOTOR_PIN1, OUTPUT);
-  pinMode(RIGHT_MOTOR_PIN2, OUTPUT);
-  pinMode(LEFT_MOTOR_PWM, OUTPUT);
-  pinMode(RIGHT_MOTOR_PWM, OUTPUT);
-  pinMode(START_BUTTON, INPUT_PULLUP);
+  pinMode(AIN1, OUTPUT);
+  pinMode(AIN2, OUTPUT);
+  pinMode(BIN1, OUTPUT);
+  pinMode(BIN2, OUTPUT);
+  pinMode(STBY, OUTPUT);
 
-  Serial.begin(9600);
-  waitForCalibration();
-}
+  pinMode(RED, OUTPUT);
+  pinMode(GREEN, OUTPUT);
+  pinMode(BLUE, OUTPUT);
+  pinMode(LIGHT, OUTPUT);
 
-void waitForCalibration() {
-  Serial.println("Нажмите кнопку для калибровки...");
-  while (digitalRead(START_BUTTON) == HIGH) delay(10);
+  digitalWrite(STBY, HIGH);
+  digitalWrite(LIGHT, HIGH);
+
+  pinMode(startButton, INPUT_PULLUP);
+  
+  digitalWrite(GREEN, HIGH);
   calibrateSensors();
+  digitalWrite(GREEN, LOW);
 }
 
 void calibrateSensors() {
-  Serial.println("Калибровка...");
-  pinMode(LED_BUILTIN, OUTPUT);
-  for (uint16_t i = 0; i < 400; i++) {
-    if (i % 40 == 0) digitalWrite(LED_BUILTIN, !digitalRead(LED_BUILTIN));
-    qtr.calibrate();
-    delay(25);
-  }
-  digitalWrite(LED_BUILTIN, LOW);
-  Serial.println("Калибровка завершена.");
-  while (digitalRead(START_BUTTON) == LOW) delay(10);
-  while (digitalRead(START_BUTTON) == HIGH) delay(10);
   delay(1000);
-  isRunning = true;
-  Serial.println("Старт!");
+
+  for (uint16_t i = 0; i < 300; i++) {
+    qtr.calibrate();
+    delay(15);
+  }
 }
 
 void loop() {
-  handleStartStopButton();
-
+  handleStartStopButton(); 
+  
   if (isRunning) {
     uint16_t position = qtr.readLineBlack(sensorValues);
 
-    // Обработка потери линии (0-7000)
-    if (position <= 200 || position >= 6800) {
-      Serial.println("Линия потеряна!");
-      smoothStop();
+    bool blackLine = true;
+    for (int i = 0; i < SensorCount; i++) {
+      if (sensorValues[i] < blackLinekLim) { 
+        blackLine = false;
+        break;
+      }
+    }
+
+    static bool wasBlackLine = false;
+    if (blackLine && !wasBlackLine) {
+      crossLineCount++;
+      wasBlackLine = true;
+    }
+    if (!blackLine) {
+      wasBlackLine = false;
+    }
+
+    if (crossLineCount >= countStopLine) {
+      stopMotors();
+      isRunning = false;
+      crossLineCount = 0;
       return;
     }
 
-    int error = position - 3500; // Центр для 8 сенсоров: 0-7000
-    Serial.print("Позиция: "); Serial.print(position);
-    Serial.print(" Ошибка: "); Serial.println(error);
+    // if (position <= minPosition || position >= maxPosition) {
+    //   stopCount++;
+    // } 
+    // else {
+    //   stopCount = 0;
+    // }
+
+    // if (stopCount >= stopLimit) {
+    //  stopMotors();
+    //  isRunning = false;
+    //  return;
+    // }
+
+    int error = position - 3500;
 
     int P = error;
     integral += error;
-    integral = constrain(integral, -1000, 1000);
+    if (integral > 1000) integral = 1000;
+    if (integral < -1000) integral = -1000;
     int I = integral;
     int D = error - lastError;
     lastError = error;
 
     int pidValue = Kp * P + Ki * I + Kd * D;
+    int leftMotorSpeed = baseSpeedL - pidValue;
+    int rightMotorSpeed = baseSpeedR + pidValue;
 
-    int leftSpeed = BASE_SPEED - pidValue;
-    int rightSpeed = BASE_SPEED + pidValue;
-    constrainSpeeds(leftSpeed, rightSpeed);
+    leftMotorSpeed = constrain(leftMotorSpeed, 0, 255);
+    rightMotorSpeed = constrain(rightMotorSpeed, 0, 255);
+  
+    driveMotors(leftMotorSpeed, rightMotorSpeed);
 
-    smoothDrive(leftSpeed, rightSpeed);
-  } else {
-    smoothStop();
+  } 
+  else {
+    digitalWrite(GREEN, LOW);
+    digitalWrite(RED, HIGH);
+    stopMotors();
   }
-
-  delay(10);
 }
 
 void handleStartStopButton() {
   static unsigned long lastPress = 0;
-  if (digitalRead(START_BUTTON) == LOW && millis() - lastPress > 500) {
+  if (digitalRead(startButton) == LOW && millis() - lastPress > 500) {
     lastPress = millis();
-    isRunning = !isRunning;
-    Serial.println(isRunning ? "Старт!" : "Стоп!");
+    
+    if (isRunning) {
+      isRunning = false;
+      digitalWrite(RED, HIGH);
+      digitalWrite(GREEN, LOW);
+    }
+    else {
+      isRunning= true;
+      delay(2000);
+      digitalWrite(GREEN, HIGH);
+      digitalWrite(RED, LOW);
+    }
   }
 }
 
-void constrainSpeeds(int &left, int &right) {
-  left = constrain(left, 0, 255);
-  right = constrain(right, 0, 255);
+void driveMotors(int leftMotorSpeed, int rightMotorSpeed) {
+  analogWrite(AIN2, leftMotorSpeed);
+
+  analogWrite(BIN1, rightMotorSpeed);
 }
 
-void smoothDrive(int targetLeft, int targetRight) {
-  int step = 5;
-  if (currentLeftSpeed < targetLeft) currentLeftSpeed += step;
-  else if (currentLeftSpeed > targetLeft) currentLeftSpeed -= step;
-
-  if (currentRightSpeed < targetRight) currentRightSpeed += step;
-  else if (currentRightSpeed > targetRight) currentRightSpeed -= step;
-
-  driveMotors(currentLeftSpeed, currentRightSpeed);
+void stopMotors() {
+  analogWrite(AIN2, 0);
+  analogWrite(BIN1, 0);
 }
 
-void smoothStop() {
-  if (currentLeftSpeed > 0) currentLeftSpeed -= 5;
-  if (currentRightSpeed > 0) currentRightSpeed -= 5;
-  if (currentLeftSpeed < 0) currentLeftSpeed = 0;
-  if (currentRightSpeed < 0) currentRightSpeed = 0;
-  driveMotors(currentLeftSpeed, currentRightSpeed);
-}
 
-void driveMotors(int leftSpeed, int rightSpeed) {
-  digitalWrite(LEFT_MOTOR_PIN1, HIGH);
-  digitalWrite(LEFT_MOTOR_PIN2, LOW);
-  digitalWrite(RIGHT_MOTOR_PIN1, HIGH);
-  digitalWrite(RIGHT_MOTOR_PIN2, LOW);
-  analogWrite(LEFT_MOTOR_PWM, leftSpeed);
-  analogWrite(RIGHT_MOTOR_PWM, rightSpeed);
-}
+
+
